8d35407d8d02ad874381dc27292333ec48f99365
DungHoang <kathymaidung@gmail.com>
2021-07-27 20:46:40 +0300

    navigate with parameters

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function ResultsShowScreen() {
+export default function ResultsShowScreen({ navigation }) {
+  const id = navigation.getParam("id");
+
+  console.log(id);
+
   return (
     <View>
       <Text>Results show</Text>
